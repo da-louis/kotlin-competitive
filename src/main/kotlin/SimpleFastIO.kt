0eb347ad8185eeed0a0bb4f5043e1274d249996b
da_louis <ruaces@gmail.com>
2020-07-17 02:27:42 +0900

    simple-fast-io: improved usability, and add workaround for StackOverFlow

--- src/main/kotlin/SimpleFastIO.kt
+++ src/main/kotlin/SimpleFastIO.kt
@@ -16,11 +16,13 @@ fun sample() = SimpleFastIO().exec {
 class SimpleFastIO {
     private val br: java.io.BufferedReader = System.`in`.bufferedReader()
     private val sb: StringBuilder = StringBuilder()
-    fun readLine() = br.readLine()!!
-    fun println() = sb.appendln()
-    fun print(any: Any) = sb.append(any)
-    fun println(any: Any) = sb.appendln(any)
-    fun <T> print(any: Iterable<T>, separator: String) = sb.append(any.joinToString(separator))
-    fun <T> println(any: Iterable<T>, separator: String) = sb.appendln(any.joinToString(separator))
-    fun exec(action: SimpleFastIO.() -> Unit) = run { action() }.run { kotlin.io.print(sb) }
+    fun readLine(): String = br.readLine()!!
+    fun println(): Unit = run { sb.appendln() }
+    fun print(any: Any): Unit = run { sb.append(any) }
+    fun println(any: Any): Unit = run { sb.appendln(any) }
+    fun <T> print(any: Iterable<T>, separator: String) = run { sb.append(any.joinToString(separator)) }
+    fun <T> println(any: Iterable<T>, separator: String) = run { sb.appendln(any.joinToString(separator)) }
+    fun exec(action: SimpleFastIO.() -> Unit) = run {
+        Thread(null, { action() }, "solve", 128 * 1024 * 1024).apply { start() }.join()
+    }.run { kotlin.io.print(sb) }
 }
